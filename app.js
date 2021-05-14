043e76a0e769ecb891f2dd753a8fefd641bfba87
egboyguda <e.gboyguda@gmail.com>
2021-05-14 13:54:48 +0800

    uploading heruko 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 const ejsMate = require('ejs-mate');
 const passport = require('passport');
